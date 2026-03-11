f60ecd9c50b494ac6bb21105c5c39c2d45aaa00c
Akshat_sh <akshatsharma1313@gmail.com>
2026-03-11 15:28:25 +0530

    fix: contact page map issue

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,13 @@ import {
 import express from 'express';
 import { join } from 'node:path';
 
-const browserDistFolder = join(import.meta.dirname, '../browser');
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const browserDistFolder = join(__dirname, '../browser');
 
 const app = express();
 const angularApp = new AngularNodeAppEngine();
@@ -51,16 +57,21 @@ app.use((req, res, next) => {
  * Start the server if this module is the main entry point.
  * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
  */
-if (isMainModule(import.meta.url)) {
-  const port = process.env['PORT'] || 4000;
-  app.listen(port, (error) => {
-    if (error) {
-      throw error;
-    }
+// if (isMainModule(import.meta.url)) {
+//   const port = process.env['PORT'] || 4000;
+//   app.listen(port, (error) => {
+//     if (error) {
+//       throw error;
+//     }
 
-    //console.log(`Node Express server listening on http://localhost:${port}`);
-  });
-}
+//     console.log(`Node Express server listening on http://localhost:${port}`);
+//   });
+// }
+const port = process.env['PORT'] || 4000;
+
+app.listen(port, () => {
+  console.log(`Node Express server listening on http://localhost:${port}`);
+});
 
 /**
  * Request handler used by the Angular CLI (for dev-server and during build) or Firebase Cloud Functions.
